845c7dce7326b7e7dc083fceebdf7a4e88359e72
Matthew McCall <m-mccall+git@outlook.com>
2026-03-24 17:28:06 -0400

    Add FetchBoost.cmake for Boost dependency management and suppress warnings

--- cmake/FetchBoost.cmake
+++ cmake/FetchBoost.cmake
@@ -3,6 +3,7 @@ FetchContent_Declare(
         URL "https://github.com/boostorg/boost/releases/download/boost-1.87.0/boost-1.87.0-cmake.tar.xz"
         URL_HASH SHA256=7da75f171837577a52bbf217e17f8ea576c7c246e4594d617bfde7fafd408be5
         DOWNLOAD_EXTRACT_TIMESTAMP ON
+        SYSTEM # Mark the dependency as a system dependency to suppress warnings from it
         EXCLUDE_FROM_ALL
         FIND_PACKAGE_ARGS CONFIG
 )
